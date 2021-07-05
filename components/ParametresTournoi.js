00a174b7d31db90430274e13d15d1627dccf637b
mathis kdio <cadio@et.esiea.fr>
2021-07-05 20:13:52 +0200

    Affichage des options retenues dans la page paramètre du tournoi

--- components/ParametresTournoi.js
+++ components/ParametresTournoi.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Button } from 'react-native'
+import { StyleSheet, View, Button, Text } from 'react-native'
 import { connect } from 'react-redux'
 
 
@@ -17,10 +17,18 @@ class ParametresTournoi extends React.Component {
   }
 
   render() {
+    let parametresTournoi = this.props.listeMatchs[this.props.listeMatchs.length - 1]
     return (
       <View style={styles.main_container} >
         <View style={styles.body_container}>
-          <View style={styles.menu_container}>
+          <View style={styles.options_container}>
+            <Text style={styles.titre}>Les options du tournois :</Text>
+            <Text style={styles.texte}>- Nombre de tours: {parametresTournoi.nbManches}</Text>
+            <Text style={styles.texte}>- Les joueurs spéciaux ne peuvent pas être dans la même équipe : {parametresTournoi.speciauxIncompatibles.toString()}</Text>
+            <Text style={styles.texte}>- Les joueurs ne peuvent pas jouer avec la même personne plusieurs fois : {parametresTournoi.memesEquipes.toString()}</Text>
+            <Text style={styles.texte}>- Les joueurs ne peuvent pas jouer contre la même personne plusieurs fois : {parametresTournoi.memesAdversaires.toString()}</Text>
+          </View>
+          <View style={styles.bouton_container}>
             <View style={styles.buttonView}>
               <Button color='red' title='Supprimer le tournoi' onPress={() => this._supprimerTournoi()}/>
             </View>
@@ -38,10 +46,25 @@ const styles = StyleSheet.create({
   main_container: {
     flex: 1
   },
-  body_container : {
+  body_container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  options_container: {
+    flex: 1,
+    marginHorizontal: 40,
+    justifyContent: 'center',
+  },
+  titre: {
+    marginBottom: 10,
+    textAlign: 'center',
+    fontSize: 24
+  },
+  texte: {
+    marginBottom: 5,
+    textAlign: 'justify',
+    fontSize: 17
   },
   buttonView: {
     marginBottom: 60
