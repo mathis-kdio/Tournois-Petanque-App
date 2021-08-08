7c37d4342bdcd7618ede3bfbcd0a784547cc6832
mathis kdio <cadio@et.esiea.fr>
2021-08-08 23:40:49 +0200

    Tournoi sans nom devient impossible si triplette et nb joueurs pas multiple de 6

--- components/InscriptionsSansNoms.js
+++ components/InscriptionsSansNoms.js
@@ -125,14 +125,31 @@ class InscriptionsSansNoms extends React.Component {
   }
 
   _boutonCommencer() {
-    let boutonActive = true
-    let boutonTitle = "Nombre de joueurs n'est pas un multiple de 2"
-    if ((this.state.nbJoueurNormaux + this.state.nbJoueurSpeciaux) % 2 == 0 && (this.state.nbJoueurNormaux + this.state.nbJoueurSpeciaux) != 0) {
-      boutonTitle = 'Commencer le tournoi'
-      boutonActive = false
+    let boutonDesactive
+    let boutonTitle = ''
+    let nbJoueurs = this.state.nbJoueurNormaux + this.state.nbJoueurSpeciaux
+    if (this.state.equipe == 'doublette') {
+      if (nbJoueurs % 2 == 0 && nbJoueurs != 0) {
+        boutonTitle = 'Commencer le tournoi'
+        boutonDesactive = false
+      }
+      else {
+        boutonTitle = "Nombre de joueurs n'est pas un multiple de 2"
+        boutonDesactive = true
+      }
+    }
+    else {
+      if (nbJoueurs % 6 == 0 && nbJoueurs >= 6) {
+        boutonTitle = 'Commencer le tournoi'
+        boutonDesactive = false
+      }
+      else {
+        boutonTitle = "En triplette, le nombre de joueurs doit être un multiple de 6"
+        boutonDesactive = true
+      }
     }
     return (
-      <Button disabled={boutonActive} color='green' title={boutonTitle} onPress={() => this._commencer()}/>
+      <Button disabled={boutonDesactive} color='green' title={boutonTitle} onPress={() => this._commencer()}/>
     )
   }
 
