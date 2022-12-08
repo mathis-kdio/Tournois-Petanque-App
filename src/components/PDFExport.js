707184d2887d67f057e9cbeed4131b5be14fb1e3
Mathis Cadio <cadio@et.esiea.fr>
2022-12-08 23:07:39 +0100

    Removal of deactivation

--- src/components/PDFExport.js
+++ src/components/PDFExport.js
@@ -207,18 +207,11 @@ class PDFExport extends React.Component {
   };
 
   _exportButton(buttonId, buttonText, affichageScore, affichageClassement) {
-    let pressableDisabled = false;
-    let opacityStyle = 1;
-    if (this.props.listeMatchs[this.props.listeMatchs.length - 1].typeTournoi == "coupe" || this.state.btnIsLoading[buttonId]) {
-      pressableDisabled = true;
-      opacityStyle = 0.7;
-    }
     return (
       <Pressable 
-        disabled={pressableDisabled}
         onPress={() => this._onPressExportBtn(buttonId, affichageScore, affichageClassement)}
       >
-        <View style={{...styles.button, opacity: opacityStyle}}>
+        <View>
           {this.state.btnIsLoading[buttonId] && <ActivityIndicator size="small" color="white" />}
           <Text style={styles.buttonText}>{buttonText}</Text>
         </View>
@@ -227,16 +220,9 @@ class PDFExport extends React.Component {
   }
 
   render() {
-    let warningText = "";
-    if (this.props.listeMatchs[this.props.listeMatchs.length - 1].typeTournoi == "coupe") {
-      warningText = "Export désactivé en mode coupe !";
-    }
     return (
       <View style={styles.main_container}>
         <View style={styles.body_container}>
-          <View style={styles.buttonView}>
-            <Text style={styles.warning_text}>{warningText}</Text>
-          </View>
           <View style={styles.buttonView}>
             {this._exportButton(0, "Exporter en PDF (sans scores)", false, false)}
           </View>
