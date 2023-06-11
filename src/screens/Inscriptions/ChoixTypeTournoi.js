67c83e9da2dba27358cfe5cb3c09f2e3ce824989
Mathis Cadio <cadio@et.esiea.fr>
2023-06-11 17:36:31 +0200

    Ajout CardButton

--- src/screens/Inscriptions/ChoixTypeTournoi.js
+++ src/screens/Inscriptions/ChoixTypeTournoi.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
-import { Box, VStack,Text, Button } from 'native-base'
+import { VStack, Text, Spacer } from 'native-base'
 import TopBarBack from 'components/TopBarBack'
+import CardButton from 'components/buttons/CardButton'
 
 class ChoixTypeTournoi extends React.Component {
   constructor(props) {
@@ -24,19 +25,38 @@ class ChoixTypeTournoi extends React.Component {
         <StatusBar backgroundColor="#0594ae"/>
         <VStack flex="1" bgColor={"#0594ae"}>
         <TopBarBack title="Type de tournoi" navigation={this.props.navigation}/>
-          <VStack flex="1" px="10" space="4">
-            <Box>
+          <VStack flex="1" px="10">
+            <Spacer/>
+            <CardButton
+              text="Type Mêlée-Démêlée"
+              icon="random"
+              navigate={() => this._navigate('mele-demele')}
+            />
+            <VStack>
+              <Text>En savoir plus:</Text>
               <Text>Choisissez vos équipes ou laisser la génération aléatoire. En tête-à-tête, doublettes ou triplettes :</Text>
-              <Button onPress={() => this._navigate('mele-demele')} bg="#1c3969">Type Mêlée-Démêlée</Button>
-            </Box>
-            <Box>
+            </VStack>
+            <Spacer/>
+            <CardButton
+              text="Type Championnat"
+              icon="table"
+              navigate={() => this._navigate('championnat')}
+            />
+            <VStack>
+              <Text>En savoir plus:</Text>
               <Text>Tous les joueurs se rencontrent à un moment dans le tournoi :</Text>
-              <Button onPress={() => this._navigate('championnat')} bg="#1c3969">Type Championnat</Button>
-            </Box>
-            <Box>
+            </VStack>
+            <Spacer/>
+            <CardButton
+              text="Type Coupe"
+              icon="trophy"
+              navigate={() => this._navigate('coupe')}
+            />
+            <VStack>
+              <Text>En savoir plus:</Text>
               <Text>Une phase de poule puis les phases finales :</Text>
-              <Button onPress={() => this._navigate('coupe')} bg="#1c3969">Type Coupe</Button>
-            </Box>
+            </VStack>
+            <Spacer/>
           </VStack>
         </VStack>
       </SafeAreaView>
