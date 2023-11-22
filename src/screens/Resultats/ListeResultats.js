942ac898c40de0f832f1c8add53aa8172299feaf
Mathis Cadio <cadio@et.esiea.fr>
2023-11-22 22:54:06 +0100

    Update ListeResultats.js

--- src/screens/Resultats/ListeResultats.js
+++ src/screens/Resultats/ListeResultats.js
@@ -4,39 +4,34 @@ import ListeResultatItem from '@components/ListeResultatItem'
 import { rankingCalc } from '@utils/ranking'
 import { withTranslation } from 'react-i18next'
 import { HStack, FlatList, Text, VStack, Divider } from '@gluestack-ui/themed'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
-import TopBarBack from '../../components/TopBarBack'
 
 class ListeResultats extends React.Component {
 
   render() {
     const { t } = this.props;
     return (
-      <SafeAreaView style={{flex: 1}}>
+      <VStack flex={1} bgColor={"#0594ae"}>
         <StatusBar backgroundColor="#0594ae"/>
-        <VStack flex={1} bgColor={"#0594ae"}>
-          {<TopBarBack title={t("resultats_classement_navigation_title")} navigation={this.props.navigation}/>}
-          <VStack flex={1} justifyContent='space-between'>
-            <HStack px={'$2'}>
-              <Text flex={2} color='white' fontSize={'$lg'}>{t("place")}</Text>
-              <Text flex={1} textAlign='center' color='white' fontSize={'$lg'}>{t("victoire")}</Text>
-              <Text flex={1} textAlign='center' color='white' fontSize={'$lg'}>{t("m_j")}</Text>
-              <Text flex={1} textAlign='right' color='white' fontSize={'$lg'}>{t("point")}</Text>
-            </HStack>
-            <Divider my="$0.5" />
-            <FlatList
-              data={rankingCalc(this.props.listeMatchs)}
-              keyExtractor={(item) => item.joueurId.toString()}
-              renderItem={({item}) => (
-                <ListeResultatItem
-                  joueur={item}
-                />
-              )}
-            />
-          </VStack>
+        <VStack flex={1} justifyContent='space-between'>
+          <HStack px={'$2'}>
+            <Text flex={2} color='white' fontSize={'$lg'}>{t("place")}</Text>
+            <Text flex={1} textAlign='center' color='white' fontSize={'$lg'}>{t("victoire")}</Text>
+            <Text flex={1} textAlign='center' color='white' fontSize={'$lg'}>{t("m_j")}</Text>
+            <Text flex={1} textAlign='right' color='white' fontSize={'$lg'}>{t("point")}</Text>
+          </HStack>
+          <Divider my="$0.5" />
+          <FlatList
+            data={rankingCalc(this.props.listeMatchs)}
+            keyExtractor={(item) => item.joueurId.toString()}
+            renderItem={({item}) => (
+              <ListeResultatItem
+                joueur={item}
+              />
+            )}
+          />
         </VStack>
-      </SafeAreaView>
+      </VStack>
     )
   }
 }
