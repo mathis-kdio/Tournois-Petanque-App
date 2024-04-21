ffa65551167c939fa77490e7d024e7903b40a6dd
Mathis Cadio <cadio@et.esiea.fr>
2024-04-21 23:10:09 +0200

    Nouvel écran changelog & utilisation <Item>

--- src/screens/Parametres/Parametres.js
+++ src/screens/Parametres/Parametres.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { expo } from '../../app.json'
+import { expo } from '../../../app.json'
 import { connect } from 'react-redux'
 import { _openURL } from '@utils/link'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { HStack, VStack, Text, FlatList, Divider, AlertDialogContent, AlertDialog, Pressable, Box, Center, Button, ButtonText, Modal, Image, ModalBackdrop, ModalContent, ModalCloseButton, ModalHeader, ModalBody, AlertDialogBody, AlertDialogHeader, AlertDialogFooter, ButtonGroup, Heading, AlertDialogCloseButton, CloseIcon, AlertDialogBackdrop, ScrollView } from '@gluestack-ui/themed'
-import { FontAwesome5 } from '@expo/vector-icons';
-import { _adsConsentShowForm } from '../utils/adMob/consentForm'
+import { VStack, Text, Divider, AlertDialogContent, AlertDialog, Box, Center, Button, ButtonText, Modal, ModalBackdrop, ModalContent, ModalCloseButton, ModalHeader, ModalBody, AlertDialogBody, AlertDialogHeader, AlertDialogFooter, ButtonGroup, Heading, AlertDialogCloseButton, CloseIcon, AlertDialogBackdrop, ScrollView } from '@gluestack-ui/themed'
+import { _adsConsentShowForm } from '../../utils/adMob/consentForm'
 import { withTranslation } from "react-i18next";
 import TopBarBack from '@components/TopBarBack'
-import ChangelogData from '@assets/ChangelogData.json'
+import Item from '@components/Item'
 
 class Parametres extends React.Component {
   constructor(props) {
@@ -18,8 +17,6 @@ class Parametres extends React.Component {
     this.crowdin = "https://crowdin.com/project/tournois-de-ptanque-gcu";
     this.state = {
       alertOpen: false,
-      modalChangelogOpen: false,
-      modalChangelogItem: undefined,
       modalLanguagesOpen: false,
     }
   }
@@ -78,28 +75,6 @@ class Parametres extends React.Component {
     this.props.dispatch(actionRemoveAllOptions);
   }
 
-  _modalChangelog() {
-    if (this.state.modalChangelogItem) {
-      let title = "Version "+this.state.modalChangelogItem.version;
-      return (
-        <Modal isOpen={this.state.modalChangelogOpen} onClose={() => this.setState({modalChangelogOpen: false})}>
-          <ModalBackdrop/>
-          <ModalContent>
-            <ModalHeader>
-              <Heading>{title}</Heading>
-              <ModalCloseButton>
-                <CloseIcon/>
-              </ModalCloseButton>
-            </ModalHeader>
-            <ModalBody>
-              <Text>{this.state.modalChangelogItem.infos}</Text>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      )
-    }
-  }
-
   _modalLanguages() {
     const { t } = this.props;
     let drapeauFrance = require('@assets/images/drapeau-france.png');
@@ -116,11 +91,11 @@ class Parametres extends React.Component {
             </ModalCloseButton>
           </ModalHeader>
           <ModalBody>
-            {this._item(t("francais"), () => this._changeLanguage("fr-FR"), undefined, "modal", drapeauFrance)}
+            <Item text={t("francais")} action={() => this._changeLanguage("fr-FR")} icon={undefined} type="modal" drapeau={drapeauFrance}/>
             <Divider/>
-            {this._item(t("anglais"), () => this._changeLanguage("en-US"), undefined, "modal", drapeauUSA)}
+            <Item text={t("anglais")} action={() => this._changeLanguage("en-US")} icon={undefined} type="modal" drapeau={drapeauUSA}/>
             <Divider/>
-            {this._item(t("polonais"), () => this._changeLanguage("pl-PL"), undefined, "modal", drapeauPologne)}
+            <Item text={t("polonais")} action={() => this._changeLanguage("pl-PL")} icon={undefined} type="modal" drapeau={drapeauPologne}/>
             <Divider/>
             <Text textAlign='center'>{t("envie_aider_traduction")}</Text>
             <Text textAlign='center' onPress={() => _openURL(this.crowdin)} color='$blue500'>{t("texte_lien_traduction")}</Text>
@@ -138,88 +113,46 @@ class Parametres extends React.Component {
     this.setState({modalLanguagesOpen: false});
   }
 
-  _item(text, action, icon, type, drapeau) {
-    let colorTxt = '$white';
-    let btnColor = 'white';
-    if (type == "danger") {
-      colorTxt = '$red500';
-      btnColor = 'red';
-    }
-    else if (type == "modal") {
-      colorTxt = '$black';
-      btnColor = 'black';
-    }
-    return (
-      <Pressable onPress={() => action()}>
-        <HStack m={'$2'} alignItems='center' justifyContent='space-between'>
-          <HStack alignItems='center'>
-            {drapeau == undefined ?
-              <FontAwesome5 name={icon} size={16} color={btnColor} style={{marginRight: 5}}/>
-              :
-              <Image source={drapeau} alt="drapeau" size='xs'/>
-            }
-            <Text fontSize={'$md'} color={colorTxt}>{text}</Text>
-          </HStack>
-          <FontAwesome5 name="arrow-right" size={20} color={btnColor}/>
-        </HStack>
-      </Pressable>
-    )
-  }
-
-  _changelogItem(item) {
-    return (
-      <VStack>
-        {this._item("Version "+item.version+" :", () => this.setState({modalChangelogOpen: true, modalChangelogItem: item}), undefined, undefined, undefined)}
-        <Divider/>
-      </VStack>
-    )
-  }
-
   render() {
     const { t } = this.props;
     return (
       <SafeAreaView style={{flex: 1}}>
-        <ScrollView height={'$1'} bgColor='#0594ae'>
+        <VStack flex={1} bgColor='#0594ae'>
           <TopBarBack title={t("parametres")} navigation={this.props.navigation}/>
-          <VStack flex={1} px={'$10'} space='lg'>
-            <VStack>
-              <Text fontSize={'$xl'} color='$white'>{t("a_propos")}</Text>
-              <Box borderWidth={'$1'} borderColor='$white' borderRadius={'$lg'}>
-                {this._item(t("voir_source_code"), () => _openURL(this.githubRepository), "code", undefined, undefined)}
-                <Divider/>
-                {this._item("tournoispetanqueapp@gmail.com", () => _openURL(this.mail), "envelope", undefined, undefined)}
-              </Box>
+          <ScrollView height={'$1'}>
+            <VStack flex={1} px={'$10'} space='lg'>
+              <VStack>
+                <Text fontSize={'$xl'} color='$white' mb={'$1'}>{t("a_propos")}</Text>
+                <Box borderWidth={'$1'} borderColor='$white' borderRadius={'$lg'}>
+                  <Item text={t("voir_source_code")} action={() => _openURL(this.githubRepository)} icon="code" type={undefined} drapeau={undefined}/>
+                  <Divider/>
+                  <Item text="tournoispetanqueapp@gmail.com" action={() => _openURL(this.mail)} icon="envelope" type={undefined} drapeau={undefined}/>
+                </Box>
+              </VStack>
+              <VStack>
+                <Text fontSize={'$xl'} color='$white' mb={'$1'}>{t("reglages")}</Text>
+                <Box borderWidth={'$1'} borderColor='$white' borderRadius={'$lg'}>
+                  <Item text={t("changer_langue")} action={() => this.setState({modalLanguagesOpen: true})} icon="language" type={undefined} drapeau={undefined}/>
+                  <Divider/>
+                  <Item text={t("modifier_consentement")} action={() => _adsConsentShowForm()} icon="ad" type={undefined} drapeau={undefined}/>
+                  <Divider/>
+                  <Item text={t("supprimer_donnees")} action={() => this.setState({alertOpen: true})} icon="trash-alt" type="danger" drapeau={undefined}/>
+                </Box>
+              </VStack>
+              <VStack>
+                <Text fontSize={'$xl'} color='$white' mb={'$1'}>{t("nouveautes")}</Text>
+                <Box borderWidth={'$1'} borderColor='$white' borderRadius={'$lg'}>
+                  <Item text={t("voir_nouveautes")} action={() => this.props.navigation.navigate('Changelog')} icon="certificate" type={undefined} drapeau={undefined}/>
+                </Box>
+              </VStack>
             </VStack>
-            <VStack>
-              <Text fontSize={'$xl'} color='$white'>{t("reglages")}</Text>
-              <Box borderWidth={'$1'} borderColor='$white' borderRadius={'$lg'}>
-                {this._item(t("changer_langue"), () => this.setState({modalLanguagesOpen: true}), "language", undefined, undefined)}
-                <Divider/>
-                {this._item(t("modifier_consentement"), () => _adsConsentShowForm(), "ad", undefined, undefined)}
-                <Divider/>
-                {this._item(t("supprimer_donnees"), () => this.setState({alertOpen: true}), "trash-alt", "danger", undefined)}
-              </Box>
-            </VStack>
-            <VStack flex={1}>
-              <Text fontSize={'$xl'} color='$white'>{t("nouveautes")}</Text>
-              <FlatList 
-                flex={1}
-                data={ChangelogData}
-                keyExtractor={(item) => item.id.toString() }
-                renderItem={({item}) => this._changelogItem(item)}
-                borderWidth={'$1'}
-                borderColor='white'
-                borderRadius={'$lg'}
-              />
-            </VStack>
-            <Center>
-              <Text textAlign='center' fontSize={'$md'} color='$white'>Version {expo.version}</Text>
-            </Center>
-          </VStack>
-        </ScrollView>
-        {this._alertDialogClearData()}
-        {this._modalChangelog()}
-        {this._modalLanguages()}
+          </ScrollView>
+          <Center>
+            <Text textAlign='center' fontSize={'$md'} color='$white'>Version {expo.version}</Text>
+          </Center>
+          {this._alertDialogClearData()}
+          {this._modalLanguages()}
+        </VStack>
       </SafeAreaView>
     )
   }
