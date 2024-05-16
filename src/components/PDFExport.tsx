8ded1f748e9a6e72f00e626e83155ca3d988c000
Mathis Cadio <mathis.cadio@gmail.com>
2024-05-16 21:45:13 +0200

    Fix imports

--- src/components/PDFExport.tsx
+++ src/components/PDFExport.tsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import * as Print from 'expo-print';
 import * as Sharing from 'expo-sharing';
 import { withTranslation } from 'react-i18next';
+import { generationPDFTournoi } from '@utils/pdf/tournoi';
+import { generationPDFCoupe } from '@utils/pdf/coupe';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { VStack, Button, ButtonSpinner, ButtonText } from '@gluestack-ui/themed';
 import TopBarBack from './TopBarBack';
