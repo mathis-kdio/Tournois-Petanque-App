466a02b0cc39a0b81f7f6848670e81952e76ef51
Mathis Cadio <mathis.cadio@gmail.com>
2024-05-18 21:59:30 +0200

    Update modeTournoi.ts

--- src/types/enums/modeTournoi.ts
+++ src/types/enums/modeTournoi.ts
@@ -2,6 +2,6 @@ export enum ModeTournoi {
   AVECNOMS = "avecNoms",
   SANSNOMS = "sansNoms",
   AVECEQUIPES = "avecEquipes",
-  SAUVEGARDE = "sauvegarde";
-  HISTORIQUE = "historique";
+  SAUVEGARDE = "sauvegarde",
+  HISTORIQUE = "historique",
 }
