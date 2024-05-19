38f73c696d8cf5b39f5546ac1958fdd001c2078f
Mathis Cadio <mathis.cadio@gmail.com>
2024-05-19 22:34:56 +0200

    date.js => ts

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,14 +1,14 @@
 import moment from 'moment/moment'
 import 'moment/locale/fr'
 
-export const dateFormatDateHeure = (date) => {
+export const dateFormatDateHeure = (date: Date) => {
   if (date != null) {
     moment.locale('fr');
     return moment(date).format('d MMMM YYYY à HH:mm:ss');
   }
 }
 
-export const dateFormatDateCompact = (date) => {
+export const dateFormatDateCompact = (date: Date) => {
   if (date != null) {
     moment.locale('fr');
     return moment(date).format('d/MM/YYYY');
