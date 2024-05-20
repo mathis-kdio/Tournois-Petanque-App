c35fd6d2fc14b74b59f66ad7831fa9fa8c7ba909
Mathis Cadio <mathis.cadio@gmail.com>
2024-05-20 23:37:16 +0200

    Ecran Compte

--- src/screens/Connexion/Compte.tsx
+++ src/screens/Connexion/Compte.tsx
@@ -35,20 +35,11 @@ class Compte extends React.Component<Props, State> {
     }
   }
 
-
   componentDidMount() {
-    if (this.state.session) {
+    supabase.auth.onAuthStateChange((event, session) => {
+      this.setState({session: session});
       this.getProfile();
-    } else {
-      /*let a = Session;
-      let b = supabase;*/
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.session !== this.state.session && this.state.session) {
-      this.getProfile();
-    }
+    })
   }
 
   async getProfile() {
@@ -64,6 +55,8 @@ class Compte extends React.Component<Props, State> {
       if (error && status !== 406) {
         throw error
       }
+
+      console.log(data)
 
       if (data) {
         this.setState({
@@ -124,45 +117,12 @@ class Compte extends React.Component<Props, State> {
           <TopBarBack title={t("mon_compte")} navigation={this.props.navigation}/>
           <VStack flex={1} px={'$10'} justifyContent='space-between'>
             <VStack>
-              <Text>{this.state.session?.user?.email}</Text>
-              <Input /*value={session?.user?.email} isDisabled*/ />
-              <Input size='md'>
-                <InputField
-                  placeholder={t("nombre_placeholder")}
-                />
-              </Input>
-            </VStack>
-            <VStack>
-              <Input /*value={username || ''}*/ />
-              <Input size='md'>
-                <InputField
-                  placeholder={t("nombre_placeholder")}
-                  onChangeText={(text) => this.setState({username: text})}
-                />
-              </Input>
-            </VStack>
-            <VStack>
-              <Input  /*value={website || ''}*/ />
-              <Input size='md'>
-                <InputField
-                  placeholder={t("nombre_placeholder")}
-                  onChangeText={(text) => this.setState({website: text})}
-                />
-              </Input>
-            </VStack>
-
-            <VStack>
-              <Button
-                onPress={() => this.updateProfile({username: this.state.username, website: this.state.website, avatar_url: this.state.avatarUrl })}
-                isDisabled={this.state.loading}
-              >
-                <ButtonText>{this.state.loading ? 'Loading ...' : 'Update'}</ButtonText>
-              </Button>
+              <Text color='$white'>{this.state.session?.user?.email}</Text>
             </VStack>
 
             <VStack>
               <Button onPress={() => supabase.auth.signOut()} >
-                <ButtonText>Sign Out</ButtonText>
+                <ButtonText>{t("se_deconnecter")}</ButtonText>
               </Button>
             </VStack>
           </VStack>
