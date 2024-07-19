e66698f114288bd3c9b91af30d33bff89760e8c2
Mathis Cadio <mathis.cadio@gmail.com>
2024-07-19 22:51:09 +0200

    configs

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,10 @@
 // This replaces `const { getDefaultConfig } = require('expo/metro-config');`
 const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+const { withNativeWind } = require('nativewind/metro');
 
 // This replaces `const config = getDefaultConfig(__dirname);`
 const config = getSentryExpoConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: './global.css' });
 
 module.exports = config;
