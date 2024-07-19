f7f15ccc25c6f5a5c23e7156cd61edafe4de1cb8
Mathis Cadio <mathis.cadio@gmail.com>
2024-07-20 01:02:49 +0200

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -6,5 +6,3 @@ const { withNativeWind } = require('nativewind/metro');
 const config = getSentryExpoConfig(__dirname);
 
 module.exports = withNativeWind(config, { input: './global.css' });
-
-module.exports = config;
