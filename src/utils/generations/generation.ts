4de35562aef5d7e225abe9de8e66ad94e44a7dfc
Mathis Cadio <mathis.cadio@gmail.com>
2024-09-21 01:11:56 +0200

    Nouvelle algorithme de mélange des ids lors de la génération

--- src/utils/generations/generation.ts
+++ src/utils/generations/generation.ts
@@ -70,7 +70,10 @@ export const calcNbMatchsParTour = (nbjoueurs: number, typeEquipes: TypeEquipes,
   return nbMatchsParTour;
 }
 
-export const shuffle = (o: number[]) => {
-  for(var j, x, i = o.length; i; j = Math.random() * i, x = o[--i], o[i] = o[j], o[j] = x);
-  return o;
-};
+export const shuffle = (array: number[]) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
