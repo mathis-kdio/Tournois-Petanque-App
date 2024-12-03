412bb6c009fef86f7ea3137eeb8cdf0b6b25b5ed
Mathis Cadio <mathis.cadio@gmail.com>
2024-12-03 23:58:37 +0100

    Update consentForm.native.ts

--- src/utils/adMob/consentForm.native.ts
+++ src/utils/adMob/consentForm.native.ts
@@ -4,19 +4,24 @@ import mobileAds from 'react-native-google-mobile-ads';
 
 export const _adsConsentForm = async (appState: AppStateStatus) => {
   if (appState === 'active') {
-    const consentInfo = await AdsConsent.requestInfoUpdate();
-    if (
-      consentInfo.isConsentFormAvailable &&
-      [AdsConsentStatus.UNKNOWN, AdsConsentStatus.REQUIRED].includes(
-        consentInfo.status,
-      )
-    ) {
-      const res = await AdsConsent.showForm();
-      if (res.status === AdsConsentStatus.OBTAINED) {
-        const adapterStatuses = await mobileAds().initialize();
-        console.log(adapterStatuses);
-      }
-    }
+    AdsConsent.requestInfoUpdate()
+      .then(async (consentInfo) => {
+        if (
+          consentInfo.isConsentFormAvailable &&
+          [AdsConsentStatus.UNKNOWN, AdsConsentStatus.REQUIRED].includes(
+            consentInfo.status,
+          )
+        ) {
+          const res = await AdsConsent.showForm();
+          if (res.status === AdsConsentStatus.OBTAINED) {
+            const adapterStatuses = await mobileAds().initialize();
+            console.log(adapterStatuses);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error('Error showing consent form:', error);
+      });
   }
 };
 
