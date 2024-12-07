7939273d50e32acf9496eb5a446f763b259ce8c1
Mathis Cadio <mathis.cadio@gmail.com>
2024-12-07 21:13:31 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,6 @@ module.exports = {
   important: 'html',
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{html,js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
-  safelist: [
-    {
-      pattern:
-        /(bg|border|text|stroke|fill)-(primary|secondary|tertiary|error|success|warning|info|typography|outline|background)-(0|50|100|200|300|400|500|600|700|800|900|950|white|gray|black|error|warning|muted|success|info|light|dark)/,
-    },
-  ],
   theme: {
     extend: {
       colors: {
