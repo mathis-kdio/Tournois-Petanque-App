a7ae50deca29824525a91e1c00ffc45b08972ccc
Mathis Cadio <mathis.cadio@gmail.com>
2025-04-28 23:44:30 +0200

    Tauri Updater Fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,42 @@
   windows_subsystem = "windows"
 )]
 
-fn main() {
+use tauri_plugin_updater::UpdaterExt;
+
+pub fn main() {
   tauri::Builder::default()
     .plugin(tauri_plugin_updater::Builder::new().build())
+    .setup(|app| {
+      let handle = app.handle().clone();
+      tauri::async_runtime::spawn(async move {
+        update(handle).await.unwrap();
+      });
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+  if let Some(update) = app.updater()?.check().await? {
+    let mut downloaded = 0;
+
+    // alternatively we could also call update.download() and update.install() separately
+    update
+      .download_and_install(
+        |chunk_length, content_length| {
+          downloaded += chunk_length;
+          println!("downloaded {downloaded} from {content_length:?}");
+        },
+        || {
+          println!("download finished");
+        },
+      )
+      .await?;
+
+    println!("update installed");
+    app.restart();
+  }
+
+  Ok(())
 }
