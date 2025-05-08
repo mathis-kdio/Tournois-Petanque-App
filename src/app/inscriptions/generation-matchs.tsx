4f20fc1b0350abd50b0df63854981f495bd80b70
Mathis Cadio <mathis.cadio@gmail.com>
2025-05-08 22:15:59 +0200

    Update generation-matchs.tsx

--- src/app/inscriptions/generation-matchs.tsx
+++ src/app/inscriptions/generation-matchs.tsx
@@ -2,7 +2,6 @@ import { Spinner } from '@/components/ui/spinner';
 import { Button, ButtonText } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
-import React from 'react';
 import { generationChampionnat } from '@utils/generations/championnat';
 import { generationCoupe } from '@utils/generations/coupe';
 import { generationMultiChances } from '@utils/generations/multiChances';
@@ -11,161 +10,131 @@ import { generationDoublettes } from '@utils/generations/tournoiDoublettes';
 import { generationTeteATete } from '@utils/generations/tournoiTeteATete';
 import { generationTriplettes } from '@utils/generations/tournoiTriplettes';
 import { uniqueValueArrayRandOrder } from '@utils/generations/generation';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { _adMobGenerationTournoiInterstitiel } from '../../components/adMob/AdMobGenerationTournoiInterstitiel';
 import { Platform } from 'react-native';
 import { EventRegister } from 'react-native-event-listeners';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { TFunction } from 'i18next';
 import { TypeEquipes } from '@/types/enums/typeEquipes';
 import { ModeTournoi } from '@/types/enums/modeTournoi';
 import { TypeTournoi } from '@/types/enums/typeTournoi';
-import { Complement } from '@/types/enums/complement';
 import { Match } from '@/types/interfaces/match';
-import { PropsFromRedux, connector } from '@/store/connector';
-import { RouteProp, StackActions } from '@react-navigation/native';
-import { InscriptionStackParamList } from '@/navigation/Navigation';
+import {
+  StackActions,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
 import { InterstitialAd } from 'react-native-google-mobile-ads';
 import TopBar from '@/components/topBar/TopBar';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
-export interface Props extends PropsFromRedux {
-  navigation: StackNavigationProp<any, any>;
-  t: TFunction;
-  route: RouteProp<InscriptionStackParamList, 'GenerationMatchs'>;
-}
+type GenerationMatchsRouteProp = {
+  params: {
+    screenStackName: string;
+  };
+};
 
-interface State {
-  isLoading: boolean;
-  isGenerationEnd: boolean;
-  isGenerationSuccess: boolean;
-  erreurSpeciaux: boolean;
-  erreurMemesEquipes: boolean;
-  adLoaded: boolean;
-  adClosed: boolean;
-}
+const GenerationMatchs = () => {
+  const { t } = useTranslation();
+  const navigation = useNavigation<StackNavigationProp<any, any>>();
+  const route = useRoute<GenerationMatchsRouteProp>();
+  const dispatch = useDispatch();
 
-class GenerationMatchs extends React.Component<Props, State> {
-  nbTours: number = 5;
-  nbPtVictoire: number = 13;
-  speciauxIncompatibles: boolean = true;
-  jamaisMemeCoequipier: boolean = true;
-  eviterMemeAdversaire: number = 50;
-  typeEquipes: TypeEquipes = TypeEquipes.DOUBLETTE;
-  typeInscription: ModeTournoi = ModeTournoi.AVECNOMS;
-  complement: Complement = undefined;
-  typeTournoi: TypeTournoi = TypeTournoi.MELEDEMELE;
-  avecTerrains: boolean = false;
-  interstitial: void | InterstitialAd;
-  listener: string | boolean;
+  const optionsTournoi = useSelector(
+    (state: any) => state.optionsTournoi.options,
+  );
+  const listesJoueurs = useSelector(
+    (state: any) => state.listesJoueurs.listesJoueurs,
+  );
+  const listeTerrains = useSelector(
+    (state: any) => state.listeTerrains.listeTerrains,
+  );
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      isGenerationEnd: false,
-      isGenerationSuccess: true,
-      erreurSpeciaux: false,
-      erreurMemesEquipes: false,
-      adLoaded: false,
-      adClosed: false,
-    };
-  }
+  const [isLoading, setIsLoading] = useState(true);
+  const [isGenerationEnd, setIsGenerationEnd] = useState(false);
+  const [isGenerationSuccess, setIsGenerationSuccess] = useState(true);
+  const [erreurSpeciaux, setErreurSpeciaux] = useState(false);
+  const [erreurMemesEquipes, setErreurMemesEquipes] = useState(false);
+  const [adLoaded, setAdLoaded] = useState(false);
+  const [adClosed, setAdClosed] = useState(false);
 
-  _ajoutMatchs = (matchs) => {
-    //this._supprimerMatchs();
+  let interstitial: void | InterstitialAd;
+  let listener: string | boolean;
+
+  const _ajoutMatchs = (matchs) => {
     const actionAjoutMatchs = { type: 'AJOUT_MATCHS', value: matchs };
-    this.props.dispatch(actionAjoutMatchs);
+    dispatch(actionAjoutMatchs);
     const actionAjoutTournoi = {
       type: 'AJOUT_TOURNOI',
       value: { tournoi: matchs },
     };
-    this.props.dispatch(actionAjoutTournoi);
+    dispatch(actionAjoutTournoi);
   };
 
-  _supprimerMatchs() {
-    const action = { type: 'SUPPR_MATCHS' };
-    this.props.dispatch(action);
-  }
+  useEffect(() => {
+    setInterstitial();
 
-  async componentDidMount() {
-    this.setInterstitial();
-    this.listener = EventRegister.addEventListener(
-      'interstitialAdEvent',
-      (data) => {
-        this.setState({ adLoaded: data.adLoaded, adClosed: data.adClosed });
-      },
-    );
-    setTimeout(() => {
-      this._lanceurGeneration();
+    listener = EventRegister.addEventListener('interstitialAdEvent', (data) => {
+      setAdLoaded(data.adLoaded);
+      setAdClosed(data.adClosed);
+    });
+
+    const timer = setTimeout(() => {
+      _lanceurGeneration();
     }, 1000);
-  }
 
-  async setInterstitial() {
+    return () => {
+      if (typeof listener === 'string') {
+        EventRegister.removeEventListener(listener);
+      }
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const setInterstitial = () => {
     const timeout = (ms: number): Promise<never> =>
       new Promise((_, reject) =>
         setTimeout(() => reject(new Error('Timeout')), ms),
       );
 
     try {
-      this.interstitial = await Promise.race([
+      Promise.race([
         _adMobGenerationTournoiInterstitiel(),
         timeout(20000),
-      ]);
+      ]).then((v) => (interstitial = v));
     } catch (error) {
       if (error instanceof Error && error.message === 'Timeout') {
-        this.setState({ adClosed: true });
+        setAdClosed(true);
       }
     }
-  }
+  };
 
-  componentDidUpdate(prevProps, prevState: State) {
-    if (
-      Platform.OS !== 'web' &&
-      this.state.isGenerationEnd &&
-      this.interstitial &&
-      this.state.adLoaded
-    ) {
-      this.interstitial.show();
+  useEffect(() => {
+    if (!isGenerationEnd) return;
+
+    if (Platform.OS !== 'web' && interstitial && adLoaded) {
+      interstitial.show();
+      return;
     }
 
-    if (
-      this.state.isGenerationEnd &&
-      (Platform.OS === 'web' || this.state.adClosed) &&
-      (!prevState.isGenerationEnd || prevState.adClosed !== this.state.adClosed)
-    ) {
-      this._displayListeMatch();
+    if (Platform.OS === 'web' || adClosed) {
+      _displayListeMatch();
+      return;
     }
-  }
+  }, [isGenerationEnd, adLoaded, adClosed]);
 
-  componentWillUnmount() {
-    if (typeof this.listener === 'string') {
-      EventRegister.removeEventListener(this.listener);
-    }
-  }
-
-  _displayListeMatch() {
-    this.props.navigation.reset({
+  const _displayListeMatch = () => {
+    navigation.reset({
       index: 0,
       routes: [{ name: 'ListeMatchsInscription' }],
     });
-  }
+  };
 
-  _lanceurGeneration() {
-    //Récupération des options que l'utilisateur a modifié ou laissé par défaut
-    this.nbTours = this.props.optionsTournoi.nbTours;
-    this.nbPtVictoire = this.props.optionsTournoi.nbPtVictoire;
-    this.speciauxIncompatibles =
-      this.props.optionsTournoi.speciauxIncompatibles;
-    this.jamaisMemeCoequipier = this.props.optionsTournoi.memesEquipes;
-    this.eviterMemeAdversaire = this.props.optionsTournoi.memesAdversaires;
-    this.typeEquipes = this.props.optionsTournoi.typeEquipes;
-    this.typeInscription = this.props.optionsTournoi.mode;
-    this.complement = this.props.optionsTournoi.complement;
-    this.typeTournoi = this.props.optionsTournoi.typeTournoi;
-    this.avecTerrains = this.props.optionsTournoi.avecTerrains;
-
-    let listeJoueurs = this.props.listesJoueurs[this.typeInscription];
+  const _lanceurGeneration = () => {
+    let typeInscription = optionsTournoi.mode;
+    let listeJoueurs = listesJoueurs[typeInscription];
     let nbjoueurs = listeJoueurs.length;
     let nbGenerationsRatee = 0;
     let nbEssaisPossibles = nbjoueurs * nbjoueurs;
@@ -176,7 +145,7 @@ class GenerationMatchs extends React.Component<Props, State> {
     // 2 si génération réussie
     //Tant que la génération échoue à cause du breaker alors on relancer
     while (nbGenerationsRatee < nbEssaisPossibles) {
-      returnType = this._generation();
+      returnType = _generation();
       if (returnType === 0 || returnType === 2) {
         break;
       } else {
@@ -185,35 +154,44 @@ class GenerationMatchs extends React.Component<Props, State> {
     }
     //Si la génération échoue trop de fois à cause du breaker alors affichage d'un message pour indiquer de changer les options
     if (nbGenerationsRatee === nbEssaisPossibles) {
-      this.setState({
-        isGenerationSuccess: false,
-        isLoading: false,
-      });
+      setIsGenerationSuccess(false);
+      setIsLoading(false);
     }
-  }
+  };
 
-  _generation() {
-    let matchs = [];
+  const _generation = () => {
+    //Récupération des options que l'utilisateur a modifié ou laissé par défaut
+    let nbTours = optionsTournoi.nbTours;
+    let avecTerrains = optionsTournoi.avecTerrains;
+    let nbPtVictoire = optionsTournoi.nbPtVictoire;
+    let speciauxIncompatibles = optionsTournoi.speciauxIncompatibles;
+    let jamaisMemeCoequipier = optionsTournoi.memesEquipes;
+    let eviterMemeAdversaire = optionsTournoi.memesAdversaires;
+    let typeEquipes = optionsTournoi.typeEquipes;
+    let complement = optionsTournoi.complement;
+    let typeTournoi = optionsTournoi.typeTournoi;
+    let typeInscription = optionsTournoi.mode;
+
+    let matchs = undefined;
     let nbMatchs = undefined;
-    let nbTours = this.nbTours;
     let erreurMemesEquipes = undefined;
     let erreurSpeciaux = undefined;
     let echecGeneration = undefined;
-    if (this.typeTournoi === TypeTournoi.MELEDEMELE) {
-      if (this.typeInscription === ModeTournoi.AVECEQUIPES) {
+    if (typeTournoi === TypeTournoi.MELEDEMELE) {
+      if (typeInscription === ModeTournoi.AVECEQUIPES) {
         ({ matchs, nbMatchs, echecGeneration } = generationAvecEquipes(
-          this.props.listesJoueurs.avecEquipes,
-          this.nbTours,
-          this.typeEquipes,
-          this.eviterMemeAdversaire,
+          listesJoueurs.avecEquipes,
+          nbTours,
+          typeEquipes,
+          eviterMemeAdversaire,
         ));
-      } else if (this.typeEquipes === TypeEquipes.TETEATETE) {
+      } else if (typeEquipes === TypeEquipes.TETEATETE) {
         ({ matchs, nbMatchs, echecGeneration } = generationTeteATete(
-          this.props.listesJoueurs[this.typeInscription],
-          this.nbTours,
-          this.eviterMemeAdversaire,
+          listesJoueurs[typeInscription],
+          nbTours,
+          eviterMemeAdversaire,
         ));
-      } else if (this.typeEquipes === TypeEquipes.DOUBLETTE) {
+      } else if (typeEquipes === TypeEquipes.DOUBLETTE) {
         ({
           matchs,
           nbMatchs,
@@ -221,14 +199,14 @@ class GenerationMatchs extends React.Component<Props, State> {
           erreurSpeciaux,
           echecGeneration,
         } = generationDoublettes(
-          this.props.listesJoueurs[this.typeInscription],
-          this.nbTours,
-          this.complement,
-          this.speciauxIncompatibles,
-          this.jamaisMemeCoequipier,
-          this.eviterMemeAdversaire,
+          listesJoueurs[typeInscription],
+          nbTours,
+          complement,
+          speciauxIncompatibles,
+          jamaisMemeCoequipier,
+          eviterMemeAdversaire,
         ));
-      } else if (this.typeEquipes === TypeEquipes.TRIPLETTE) {
+      } else if (typeEquipes === TypeEquipes.TRIPLETTE) {
         ({
           matchs,
           nbMatchs,
@@ -236,62 +214,60 @@ class GenerationMatchs extends React.Component<Props, State> {
           erreurSpeciaux,
           echecGeneration,
         } = generationTriplettes(
-          this.props.listesJoueurs[this.typeInscription],
-          this.nbTours,
-          this.complement,
-          this.speciauxIncompatibles,
-          this.jamaisMemeCoequipier,
-          this.eviterMemeAdversaire,
+          listesJoueurs[typeInscription],
+          nbTours,
+          complement,
+          speciauxIncompatibles,
+          jamaisMemeCoequipier,
+          eviterMemeAdversaire,
         ));
       } else {
         echecGeneration = true;
       }
-    } else if (this.typeTournoi === TypeTournoi.COUPE) {
+    } else if (typeTournoi === TypeTournoi.COUPE) {
       ({ matchs, nbTours, nbMatchs } = generationCoupe(
-        this.props.optionsTournoi,
-        this.props.listesJoueurs.avecEquipes,
+        optionsTournoi,
+        listesJoueurs.avecEquipes,
       ));
-    } else if (this.typeTournoi === TypeTournoi.CHAMPIONNAT) {
+    } else if (typeTournoi === TypeTournoi.CHAMPIONNAT) {
       ({ matchs, nbTours, nbMatchs } = generationChampionnat(
-        this.props.optionsTournoi,
-        this.props.listesJoueurs.avecEquipes,
+        optionsTournoi,
+        listesJoueurs.avecEquipes,
       ));
-    } else if (this.typeTournoi === TypeTournoi.MULTICHANCES) {
+    } else if (typeTournoi === TypeTournoi.MULTICHANCES) {
       ({ matchs, nbTours, nbMatchs } = generationMultiChances(
-        this.props.listesJoueurs[this.typeInscription],
-        this.typeEquipes,
+        listesJoueurs[typeInscription],
+        typeEquipes,
       ));
     } else {
       echecGeneration = true;
     }
-    if (erreurMemesEquipes || erreurSpeciaux) {
-      this.setState({
-        erreurMemesEquipes: erreurMemesEquipes,
-        erreurSpeciaux: erreurSpeciaux,
-        isLoading: false,
-      });
+    if (erreurMemesEquipes) {
+      setErreurMemesEquipes(erreurMemesEquipes);
+      setIsLoading(false);
       return 0;
     }
-    if (echecGeneration) {
+    if (erreurSpeciaux) {
+      setErreurSpeciaux(erreurSpeciaux);
+      setIsLoading(false);
+      return 0;
+    }
+    if (!matchs || echecGeneration) {
       return 1;
     }
 
     //attributions des terrains
-    if (this.avecTerrains) {
+    if (avecTerrains) {
       let manche = matchs[0].manche;
-      let arrRandIdsTerrains = uniqueValueArrayRandOrder(
-        this.props.listeTerrains.length,
-      );
+      let arrRandIdsTerrains = uniqueValueArrayRandOrder(listeTerrains.length);
       let i = 0;
       matchs.forEach((match: Match) => {
         if (match.manche !== manche) {
           manche = match.manche;
-          arrRandIdsTerrains = uniqueValueArrayRandOrder(
-            this.props.listeTerrains.length,
-          );
+          arrRandIdsTerrains = uniqueValueArrayRandOrder(listeTerrains.length);
           i = 0;
         }
-        match.terrain = this.props.listeTerrains[arrRandIdsTerrains[i]];
+        match.terrain = listeTerrains[arrRandIdsTerrains[i]];
         i++;
       });
     }
@@ -301,33 +277,30 @@ class GenerationMatchs extends React.Component<Props, State> {
       tournoiID: undefined,
       nbTours: nbTours,
       nbMatchs: nbMatchs,
-      nbPtVictoire: this.nbPtVictoire,
-      speciauxIncompatibles: this.speciauxIncompatibles,
-      memesEquipes: this.jamaisMemeCoequipier,
-      memesAdversaires: this.eviterMemeAdversaire,
-      typeEquipes: this.typeEquipes,
-      complement: this.complement,
-      typeTournoi: this.typeTournoi,
-      listeJoueurs: this.props.listesJoueurs[this.typeInscription].slice(),
-      avecTerrains: this.avecTerrains,
+      nbPtVictoire: nbPtVictoire,
+      speciauxIncompatibles: speciauxIncompatibles,
+      memesEquipes: jamaisMemeCoequipier,
+      memesAdversaires: eviterMemeAdversaire,
+      typeEquipes: typeEquipes,
+      complement: complement,
+      typeTournoi: typeTournoi,
+      listeJoueurs: listesJoueurs[typeInscription].slice(),
+      avecTerrains: avecTerrains,
     });
 
     //Ajout dans le store
-    this._ajoutMatchs(matchs);
+    _ajoutMatchs(matchs);
 
     //Désactivation de l'affichage du _displayLoading
-    this.setState({
-      isLoading: false,
-      isGenerationEnd: true,
-    });
+    setIsLoading(false);
+    setIsGenerationEnd(true);
 
     //Si génération valide alors return 2
     return 2;
-  }
+  };
 
-  _displayLoading() {
-    const { t } = this.props;
-    if (this.state.isLoading) {
+  const _displayLoading = () => {
+    if (isLoading) {
       return (
         <VStack>
           <Spinner size={'large'} className="text-[#ffda00]" />
@@ -337,11 +310,11 @@ class GenerationMatchs extends React.Component<Props, State> {
     } else {
       let textInfo = t('erreur_generation_options');
       let textError = '';
-      if (!this.state.isGenerationSuccess) {
+      if (!isGenerationSuccess) {
         textError = t('erreur_generation_options_regles');
-      } else if (this.state.erreurSpeciaux) {
+      } else if (erreurSpeciaux) {
         textError = t('erreur_generation_joueurs_speciaux');
-      } else if (this.state.erreurMemesEquipes) {
+      } else if (erreurMemesEquipes) {
         textError = t('erreur_generation_regle_equipes');
       } else {
         textInfo = t('chargement_publicite');
@@ -350,34 +323,31 @@ class GenerationMatchs extends React.Component<Props, State> {
         <VStack>
           <Text className="text-white">{textInfo}</Text>
           <Text className="text-white">{textError}</Text>
-          <Button action="primary" onPress={() => this._retourInscription()}>
+          <Button action="primary" onPress={() => _retourInscription()}>
             <ButtonText>{t('retour_inscription')}</ButtonText>
           </Button>
         </VStack>
       );
     }
-  }
+  };
 
-  _retourInscription() {
+  const _retourInscription = () => {
     const popToAction = StackActions.popTo(
-      this.props.route.params.screenStackName,
+      route.params.screenStackName,
     );
-    this.props.navigation.dispatch(popToAction);
-  }
+    navigation.dispatch(popToAction);
+  };
 
-  render() {
-    const { t } = this.props;
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <VStack className="flex-1 bg-[#0594ae]">
-          <TopBar title={t('generation_matchs_navigation_title')} />
-          <VStack className="flex-1 px-10 justify-center items-center">
-            {this._displayLoading()}
-          </VStack>
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <VStack className="flex-1 bg-[#0594ae]">
+        <TopBar title={t('generation_matchs_navigation_title')} />
+        <VStack className="flex-1 px-10 justify-center items-center">
+          {_displayLoading()}
         </VStack>
-      </SafeAreaView>
-    );
-  }
-}
+      </VStack>
+    </SafeAreaView>
+  );
+};
 
-export default connector(withTranslation()(GenerationMatchs));
+export default GenerationMatchs;
