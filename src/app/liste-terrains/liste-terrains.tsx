9b850f05e4ff92423052cf424d691810a23d06f4
Mathis Cadio <mathis.cadio@gmail.com>
2025-05-08 20:50:41 +0200

    Update liste-terrains.tsx

--- src/app/liste-terrains/liste-terrains.tsx
+++ src/app/liste-terrains/liste-terrains.tsx
@@ -2,53 +2,54 @@ import { FlatList } from '@/components/ui/flat-list';
 import { Button, ButtonText } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
-import React from 'react';
 import ListeTerrainItem from '@components/ListeTerrainItem';
 import { calcNbMatchsParTour } from '@utils/generations/generation';
-import { withTranslation } from 'react-i18next';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import TopBarBack from '@/components/topBar/TopBarBack';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { TFunction } from 'i18next';
-import { PropsFromRedux, connector } from '@/store/connector';
 import { Terrain } from '@/types/interfaces/terrain';
 import { ListRenderItem } from 'react-native';
-import { RouteProp } from '@react-navigation/native';
-import { InscriptionStackParamList } from '@/navigation/Navigation';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
-export interface Props extends PropsFromRedux {
-  navigation: StackNavigationProp<any, any>;
-  t: TFunction;
-  route: RouteProp<InscriptionStackParamList, 'ListeTerrains'>;
-}
+type ListeTerrainsRouteProp = {
+  params: {
+    screenStackName: string;
+  };
+};
 
-interface State {}
+const ListeTerrains = () => {
+  const { t } = useTranslation();
+  const navigation = useNavigation<StackNavigationProp<any, any>>();
+  const route = useRoute<ListeTerrainsRouteProp>();
+  const dispatch = useDispatch();
 
-class ListeTerrains extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {};
-  }
+  const listesJoueurs = useSelector(
+    (state: any) => state.listesJoueurs.listesJoueurs,
+  );
+  const listeTerrains = useSelector(
+    (state: any) => state.listeTerrains.listeTerrains,
+  );
+  const optionsTournoi = useSelector(
+    (state: any) => state.optionsTournoi.options,
+  );
 
-  _ajoutTerrains() {
+  const _ajoutTerrains = () => {
     const ajoutTerrain = { type: 'AJOUT_TERRAIN', value: [] };
-    this.props.dispatch(ajoutTerrain);
-  }
+    dispatch(ajoutTerrain);
+  };
 
-  _ajoutTerrainButton() {
-    const { t } = this.props;
+  const _ajoutTerrainButton = () => {
     return (
-      <Button action="primary" onPress={() => this._ajoutTerrains()}>
+      <Button action="primary" onPress={() => _ajoutTerrains()}>
         <ButtonText>{t('ajouter_terrain')}</ButtonText>
       </Button>
     );
-  }
+  };
 
-  _commencerButton() {
-    const { t } = this.props;
-    const { typeEquipes, mode, typeTournoi, complement } =
-      this.props.optionsTournoi;
-    const { listesJoueurs, listeTerrains } = this.props;
+  const _commencerButton = () => {
+    const { typeEquipes, mode, typeTournoi, complement } = optionsTournoi;
     const nbJoueurs = listesJoueurs[mode].length;
     const nbTerrainsNecessaires = calcNbMatchsParTour(
       nbJoueurs,
@@ -63,56 +64,53 @@ class ListeTerrains extends React.Component<Props, State> {
       <Button
         isDisabled={disabled}
         action="positive"
-        onPress={() => this._commencer()}
+        onPress={() => _commencer()}
       >
         <ButtonText>{title}</ButtonText>
       </Button>
     );
-  }
+  };
 
-  _commencer() {
-    this.props.navigation.navigate({
+  const _commencer = () => {
+    navigation.navigate({
       name: 'GenerationMatchs',
       params: {
-        screenStackName: this.props.route.params.screenStackName,
+        screenStackName: route.params.screenStackName,
       },
     });
-  }
+  };
 
-  render() {
-    const { t, listeTerrains } = this.props;
-    const renderItem: ListRenderItem<Terrain> = ({ item }) => (
-      <ListeTerrainItem terrain={item} />
-    );
+  const renderItem: ListRenderItem<Terrain> = ({ item }) => (
+    <ListeTerrainItem terrain={item} />
+  );
 
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <VStack className="flex-1 bg-[#0594ae]">
-          <TopBarBack
-            title={t('liste_terrains_navigation_title')}
-            navigation={this.props.navigation}
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <VStack className="flex-1 bg-[#0594ae]">
+        <TopBarBack
+          title={t('liste_terrains_navigation_title')}
+          navigation={navigation}
+        />
+        <Text className="text-white text-xl text-center">
+          {t('nombre_terrains', { nb: listeTerrains.length })}
+        </Text>
+        <VStack className="flex-1 my-2">
+          <FlatList
+            persistentScrollbar={true}
+            data={listeTerrains}
+            initialNumToRender={20}
+            keyExtractor={(item: Terrain) => item.id.toString()}
+            renderItem={renderItem}
+            className="h-1"
           />
-          <Text className="text-white text-xl text-center">
-            {t('nombre_terrains', { nb: listeTerrains.length })}
-          </Text>
-          <VStack className="flex-1 my-2">
-            <FlatList
-              persistentScrollbar={true}
-              data={listeTerrains}
-              initialNumToRender={20}
-              keyExtractor={(item: Terrain) => item.id.toString()}
-              renderItem={renderItem}
-              className="h-1"
-            />
-          </VStack>
-          <VStack space="lg" className="px-10">
-            {this._ajoutTerrainButton()}
-            {this._commencerButton()}
-          </VStack>
         </VStack>
-      </SafeAreaView>
-    );
-  }
-}
+        <VStack space="lg" className="px-10">
+          {_ajoutTerrainButton()}
+          {_commencerButton()}
+        </VStack>
+      </VStack>
+    </SafeAreaView>
+  );
+};
 
-export default connector(withTranslation()(ListeTerrains));
+export default ListeTerrains;
