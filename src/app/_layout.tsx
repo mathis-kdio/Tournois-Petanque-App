8795c8643e04b0e04d5c5a1d5eed6688b38cfc1b
Mathis Cadio <mathis.cadio@gmail.com>
2025-05-22 23:48:13 +0200

    App.tsx devient RootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,18 @@
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/navigation/gesture-handler'; //https://reactnavigation.org/docs/stack-navigator/
 import 'expo-dev-client';
-import React from 'react';
-import Navigation from '@navigation/Navigation';
 import { Provider } from 'react-redux';
 import Store from '@store/configureStore';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/es/integration/react';
-import {
-  NavigationContainer,
-  createNavigationContainerRef,
-} from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { I18nextProvider } from 'react-i18next';
 import * as Sentry from '@sentry/react-native';
-import i18n from './i18n';
-import './global.css';
+import i18n from '../../i18n';
+import '../../global.css';
 import '@expo/metro-runtime'; //Fast-refresh web
 import { AuthProvider } from '@/components/supabase/SessionProvider';
+import { Stack } from 'expo-router';
 
 const reactNavigationIntegration = Sentry.reactNavigationIntegration({
   enableTimeToInitialDisplay: true,
@@ -34,36 +29,26 @@ Sentry.init({
   attachStacktrace: true,
 });
 
-class App extends React.Component {
-  navigationRef = createNavigationContainerRef<any>(); //TODO tmp changer any
-  render() {
-    let persistor = persistStore(Store);
-    return (
-      <Provider store={Store}>
-        <PersistGate persistor={persistor}>
-          <AuthProvider>
-            <GluestackUIProvider mode="light">
-              <NavigationContainer
-                ref={this.navigationRef}
-                onReady={() =>
-                  reactNavigationIntegration.registerNavigationContainer(
-                    this.navigationRef,
-                  )
-                }
-              >
-                <I18nextProvider i18n={i18n} defaultNS={'common'}>
-                  <Navigation />
-                  <StatusBar style="light" backgroundColor="#0594ae" />
-                </I18nextProvider>
-              </NavigationContainer>
-            </GluestackUIProvider>
-          </AuthProvider>
-        </PersistGate>
-      </Provider>
-    );
-  }
+export default function RootLayout() {
+  let persistor = persistStore(Store);
+
+  return (
+    <Provider store={Store}>
+      <PersistGate persistor={persistor}>
+        <AuthProvider>
+          <GluestackUIProvider mode="light">
+            <I18nextProvider i18n={i18n} defaultNS={'common'}>
+              <Stack />
+              <StatusBar style="light" backgroundColor="#0594ae" />
+            </I18nextProvider>
+          </GluestackUIProvider>
+        </AuthProvider>
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default Sentry.wrap(App, {
+
+/*Sentry.wrap(App, {
   touchEventBoundaryProps: { labelName: 'accessibilityLabel' },
-});
+});*/
