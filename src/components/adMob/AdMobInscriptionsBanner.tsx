54956af2aae786b657c5c3c7f6ab97d680c5e88d
Mathis Cadio <mathis.cadio@gmail.com>
2025-06-03 23:29:20 +0200

    Functionnal component pour bannière

--- src/components/adMob/AdMobInscriptionsBanner.tsx
+++ src/components/adMob/AdMobInscriptionsBanner.tsx
@@ -1,19 +1,6 @@
-import * as React from 'react';
-
-export interface Props {}
-
-interface State {}
-
 //Cas web => voir AdMobInscriptionsBanner.native.js pour android et ios
-class AdMobInscriptionsBanner extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return <></>;
-  }
-}
+const AdMobInscriptionsBanner = () => {
+  return <></>;
+};
 
 export default AdMobInscriptionsBanner;
