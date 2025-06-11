62126b792fd11d25ef07844d41fed84c5274d3dd
Mathis Cadio <mathis.cadio@gmail.com>
2025-06-11 23:09:48 +0200

    StackLayout

--- src/app/listes-joueurs/_layout.tsx
+++ src/app/listes-joueurs/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from 'expo-router';
 
 export default function StackLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="create-liste-joueurs" />
+    </Stack>
+  );
 }
