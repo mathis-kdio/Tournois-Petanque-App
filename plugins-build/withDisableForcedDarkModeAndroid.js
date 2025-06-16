24be4b29e0725851b97567b54f8db0fa0651ff8e
Mathis Cadio <mathis.cadio@gmail.com>
2025-06-16 23:44:56 +0200

    Fix lint

--- plugins-build/withDisableForcedDarkModeAndroid.js
+++ plugins-build/withDisableForcedDarkModeAndroid.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // disable forced dark mode to prevent weird color changes on
 // certain android devices (Xiaomi MIUI and others enforcing dark mode with view analyzing)
 const config_plugins_1 = require("@expo/config-plugins");
-const withDisableForcedDarkModeAndroid = config => {
-    return (0, config_plugins_1.withAndroidStyles)(config, androidStylesConfig => {
+const withDisableForcedDarkModeAndroid = (config) => {
+    return (0, config_plugins_1.withAndroidStyles)(config, (androidStylesConfig) => {
         const styles = androidStylesConfig.modResults;
         androidStylesConfig.modResults = config_plugins_1.AndroidConfig.Styles.assignStylesValue(styles, {
             add: true,
--- src/plugins/withDisableForcedDarkModeAndroid.ts
+++ src/plugins/withDisableForcedDarkModeAndroid.ts
@@ -1,16 +1,23 @@
 // disable forced dark mode to prevent weird color changes on
 // certain android devices (Xiaomi MIUI and others enforcing dark mode with view analyzing)
-import { AndroidConfig, withAndroidStyles, ConfigPlugin } from '@expo/config-plugins';
+import {
+  AndroidConfig,
+  withAndroidStyles,
+  ConfigPlugin,
+} from '@expo/config-plugins';
 
-const withDisableForcedDarkModeAndroid: ConfigPlugin = config => {
-  return withAndroidStyles(config, androidStylesConfig => {
+const withDisableForcedDarkModeAndroid: ConfigPlugin = (config) => {
+  return withAndroidStyles(config, (androidStylesConfig) => {
     const styles = androidStylesConfig.modResults;
-    androidStylesConfig.modResults = AndroidConfig.Styles.assignStylesValue(styles, {
-      add: true,
-      parent: AndroidConfig.Styles.getAppThemeGroup(),
-      name: `android:forceDarkAllowed`,
-      value: 'false',
-    });
+    androidStylesConfig.modResults = AndroidConfig.Styles.assignStylesValue(
+      styles,
+      {
+        add: true,
+        parent: AndroidConfig.Styles.getAppThemeGroup(),
+        name: `android:forceDarkAllowed`,
+        value: 'false',
+      },
+    );
     return androidStylesConfig;
   });
 };
