cd353d8670324f89d90b3442e0ec7ba5e219945e
Mathis Cadio <mathis.cadio@gmail.com>
2025-09-26 23:00:27 +0200

    Update expo-file-system

--- src/app/tournoi/pdf-export.tsx
+++ src/app/tournoi/pdf-export.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import * as Print from 'expo-print';
 import * as Sharing from 'expo-sharing';
 import * as IntentLauncher from 'expo-intent-launcher';
-import * as FileSystem from 'expo-file-system';
+import { File, Paths } from 'expo-file-system';
 import { useTranslation } from 'react-i18next';
 import { generationPDFTournoi } from '@utils/pdf/tournoi';
 import { generationPDFCoupe } from '@utils/pdf/coupe';
@@ -106,22 +106,19 @@ const PDFExport = () => {
 
       const date = dateFormatDateFileName(infosTournoi.creationDate);
       const newFileName = `tournoi-petanque-${infosTournoi.tournoiId}-${date}.pdf`;
-      const newUri = FileSystem.cacheDirectory + newFileName;
-      await FileSystem.moveAsync({
-        from: uri,
-        to: newUri,
-      });
+      const file = new File(uri);
+      file.move(Paths.cache);
+      file.rename(newFileName);
 
       if (Platform.OS === 'android') {
-        let contentUri = await FileSystem.getContentUriAsync(newUri);
         IntentLauncher.startActivityAsync('android.intent.action.VIEW', {
-          data: contentUri,
+          data: file.uri,
           flags: 1,
           type: 'application/pdf',
         }).then(() => _toggleLoading());
       } else if (Platform.OS === 'ios') {
         if (await Sharing.isAvailableAsync()) {
-          Sharing.shareAsync(newUri).then(() => _toggleLoading());
+          Sharing.shareAsync(file.uri).then(() => _toggleLoading());
         } else {
           _toggleLoading();
         }
