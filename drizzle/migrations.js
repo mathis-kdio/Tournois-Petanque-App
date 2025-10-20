039e8af1c2c16ea69314584fdb2ed93e5b6c30e7
Mathis Cadio <mathis.cadio@gmail.com>
2025-10-20 23:13:23 +0200

    reset migration

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,7 +1,7 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_brief_james_howlett.sql';
+import m0000 from './0000_violet_terrax.sql';
 
 export default {
   journal,
