76bb245c6da4066ae60eaacef58aa21d4ca0f97d
Mathis Cadio <mathis.cadio@gmail.com>
2025-10-25 18:12:34 +0200

    new migration

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_violet_terrax.sql';
 import m0001 from './0001_true_blink.sql';
+import m0002 from './0002_loving_magneto.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
