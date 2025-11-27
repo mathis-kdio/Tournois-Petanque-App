a2dbbfdc793a3b7e06fc9a22a4ec4fc463f9445f
Mathis Cadio <mathis.cadio@gmail.com>
2025-11-27 23:25:20 +0100

    Ajout tauri opener

--- src/utils/pdf/generate/genererPdf.ts
+++ src/utils/pdf/generate/genererPdf.ts
@@ -4,27 +4,33 @@ import {
   BaseDirectory,
   writeFile,
 } from '@tauri-apps/plugin-fs';
+import { openPath } from '@tauri-apps/plugin-opener';
+import { downloadDir } from '@tauri-apps/api/path';
 import html2pdf from 'html2pdf.js';
 
-export const genererPdf = async (newFileName: string, html: string) => {
-  const fileExists = await exists(newFileName, {
+export const genererPdf = async (fileName: string, html: string) => {
+  const fileExists = await exists(fileName, {
     baseDir: BaseDirectory.Download,
   });
   if (fileExists) {
-    await remove(newFileName, { baseDir: BaseDirectory.Download });
+    await remove(fileName, { baseDir: BaseDirectory.Download });
   }
 
   const worker = html2pdf()
     .from(html)
     .set({
       margin: 10,
-      filename: newFileName,
+      filename: fileName,
       html2canvas: { scale: 2 },
       jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
     });
   const pdf = await worker.outputPdf('arraybuffer');
 
-  await writeFile(newFileName, pdf, {
+  await writeFile(fileName, pdf, {
     baseDir: BaseDirectory.Download,
   });
+
+  const downloads = await downloadDir();
+  const filePath = `${downloads}\\${fileName}`;
+  await openPath(filePath);
 };
