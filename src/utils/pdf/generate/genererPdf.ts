290e866c88a31def0c76688e7e5b003adbc3b0bb
Mathis Cadio <mathis.cadio@gmail.com>
2025-12-01 22:37:58 +0100

    génération pdf pour web et tauri

--- src/utils/pdf/generate/genererPdf.ts
+++ src/utils/pdf/generate/genererPdf.ts
@@ -8,7 +8,39 @@ import { openPath } from '@tauri-apps/plugin-opener';
 import { downloadDir, sep } from '@tauri-apps/api/path';
 import html2pdf from 'html2pdf.js';
 
-export const genererPdf = async (fileName: string, html: string) => {
+export const genererPdf = async (
+  fileName: string,
+  html: string,
+): Promise<void> => {
+  if ('__TAURI_INTERNALS__' in window) {
+    genererPdfTauri(fileName, html);
+  } else {
+    genererPdfWeb(fileName, html);
+  }
+};
+
+const genererPdfWeb = (fileName: string, html: string): void => {
+  const pW = window.open(
+    '',
+    '',
+    `width=${screen.availWidth},height=${screen.availHeight}`,
+  );
+  if (pW === null) {
+    throw new Error("Impossible d'ouvrir la fenêtre d'impression.");
+  }
+
+  pW.document.write(html);
+  pW.document.title = fileName;
+  pW.onafterprint = () => {
+    pW.close();
+  };
+  pW.print();
+};
+
+const genererPdfTauri = async (
+  fileName: string,
+  html: string,
+): Promise<void> => {
   const fileExists = await exists(fileName, {
     baseDir: BaseDirectory.Download,
   });
