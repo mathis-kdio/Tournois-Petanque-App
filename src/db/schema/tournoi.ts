c394284b51a013dbcb31a8e869e036cb9d0c6366
Mathis Cadio <mathis.cadio@gmail.com>
2026-01-30 23:00:54 +0100

    Table tournoi

--- src/db/schema/tournoi.ts
+++ src/db/schema/tournoi.ts
@@ -1,14 +1,35 @@
+import { ModeTournoi } from '@/types/enums/modeTournoi';
+import { TypeEquipes } from '@/types/enums/typeEquipes';
+import { TypeTournoi } from '@/types/enums/typeTournoi';
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const tournoi = sqliteTable('tournoi', {
   id: integer('id').primaryKey(),
-  name: text('name'),
-  option1: text('option1'),
-  option2: text('option2'),
-  option3: text('option3'),
-  createAt: integer('create_at'),
-  updatedAt: integer('updated_at'),
-  synced: integer('synced').default(0),
+  name: text('name').notNull(),
+  nbTours: integer().notNull(),
+  nbMatchs: integer().notNull(),
+  nbPtVictoire: integer().notNull(),
+  speciauxIncompatibles: integer({ mode: 'boolean' }).notNull(),
+  memesEquipes: integer({ mode: 'boolean' }).notNull(),
+  memesAdversaires: integer().notNull(),
+  typeEquipes: text({
+    enum: [TypeEquipes.DOUBLETTE, TypeEquipes.TETEATETE, TypeEquipes.TRIPLETTE],
+  }).notNull(),
+  typeTournoi: text({
+    enum: [
+      TypeTournoi.CHAMPIONNAT,
+      TypeTournoi.COUPE,
+      TypeTournoi.MELEDEMELE,
+      TypeTournoi.MELEE,
+      TypeTournoi.MULTICHANCES,
+    ],
+  }).notNull(),
+  avecTerrains: integer({ mode: 'boolean' }).notNull(),
+  mode: text({
+    enum: [ModeTournoi.AVECEQUIPES, ModeTournoi.AVECNOMS, ModeTournoi.SANSNOMS],
+  }).notNull(),
+  createAt: integer('create_at').notNull(),
+  updatedAt: integer('updated_at').notNull(),
 });
 
 export type Tournoi = typeof tournoi.$inferSelect;
