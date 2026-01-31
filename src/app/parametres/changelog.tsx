96723aeae53a21ddee1f95cc71e8764338625a25
Mathis Cadio <mathis.cadio@gmail.com>
2026-01-31 22:19:31 +0100

    Refacto changelog

--- src/app/parametres/changelog.tsx
+++ src/app/parametres/changelog.tsx
@@ -1,109 +1,12 @@
-import { CloseIcon, Icon } from '@/components/ui/icon';
-import { Heading } from '@/components/ui/heading';
-
-import {
-  Modal,
-  ModalBackdrop,
-  ModalContent,
-  ModalCloseButton,
-  ModalHeader,
-  ModalBody,
-} from '@/components/ui/modal';
-
-import { Divider } from '@/components/ui/divider';
-import { FlatList } from '@/components/ui/flat-list';
-import { Text } from '@/components/ui/text';
-import { VStack } from '@/components/ui/vstack';
-import { useState } from 'react';
+import Changelog from '@/screens/changelog';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useTranslation } from 'react-i18next';
-import TopBarBack from '@/components/topBar/TopBarBack';
-import ChangelogData from '@assets/ChangelogData.json';
-import Item from '@components/Item';
-import { Changelog as ChangelogInterface } from '@/types/interfaces/changelog';
-import { ListRenderItem } from 'react-native';
 
-const Changelog = () => {
-  const [modalChangelogOpen, openModalChangelog] = useState(false);
-  const [modalChangelogItem, setModalChangelogItem] =
-    useState<ChangelogInterface>();
-
-  const { t } = useTranslation(['common', 'changelog']);
-
-  const _modalChangelog = () => {
-    if (modalChangelogItem) {
-      let title = t('version') + ' ' + modalChangelogItem.version;
-      return (
-        <Modal
-          isOpen={modalChangelogOpen}
-          onClose={() => openModalChangelog(false)}
-        >
-          <ModalBackdrop />
-          <ModalContent className="max-h-5/6">
-            <ModalHeader>
-              <Heading className="color-custom-text-modal">{title}</Heading>
-              <ModalCloseButton>
-                <Icon
-                  as={CloseIcon}
-                  size="md"
-                  className="stroke-background-400 group-[:hover]/modal-close-button:stroke-background-700 group-[:active]/modal-close-button:stroke-background-900 group-[:focus-visible]/modal-close-button:stroke-background-900"
-                />
-              </ModalCloseButton>
-            </ModalHeader>
-            <ModalBody>
-              <Text>
-                {t(`${modalChangelogItem.id}.infos`, {
-                  ns: 'changelog',
-                  returnObjects: true,
-                  joinArrays: '\n',
-                })}
-              </Text>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      );
-    }
-  };
-
-  const _changelogItem = (item: ChangelogInterface) => {
-    return (
-      <VStack>
-        <Item
-          text={t('version') + ' ' + item.version + ' :'}
-          action={() => {
-            openModalChangelog(true);
-            setModalChangelogItem(item);
-          }}
-          icon={''}
-          type={''}
-          drapeau={undefined}
-        />
-        <Divider />
-      </VStack>
-    );
-  };
-
-  const renderItem: ListRenderItem<ChangelogInterface> = ({ item }) =>
-    _changelogItem(item);
+const ChangelogScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <VStack className="flex-1 bg-custom-background">
-        <TopBarBack title={t('nouveautes')} />
-        <VStack className="flex-1 px-10">
-          <Text className="text-xl text-typography-white mb-1">
-            {t('nouveautes')}
-          </Text>
-          <FlatList
-            data={Object.values(ChangelogData).reverse()}
-            keyExtractor={(item: ChangelogInterface) => item.id.toString()}
-            renderItem={renderItem}
-            className="h-1 border border-custom-bg-inverse rounded-lg"
-          />
-        </VStack>
-      </VStack>
-      {_modalChangelog()}
+      <Changelog />
     </SafeAreaView>
   );
 };
 
-export default Changelog;
+export default ChangelogScreen;
---
+++ src/screens/changelog/components/ChangelogModal.tsx
@@ -0,0 +1,60 @@
+import { CloseIcon, Icon } from '@/components/ui/icon';
+import { Heading } from '@/components/ui/heading';
+import {
+  Modal,
+  ModalBackdrop,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+} from '@/components/ui/modal';
+import { Text } from '@/components/ui/text';
+import { useTranslation } from 'react-i18next';
+import { Changelog as ChangelogInterface } from '@/types/interfaces/changelog';
+
+export interface Props {
+  modalChangelogItem: ChangelogInterface;
+  modalChangelogOpen: boolean;
+  openModalChangelog: (value: React.SetStateAction<boolean>) => void;
+}
+
+const ChangelogModal: React.FC<Props> = ({
+  modalChangelogItem,
+  modalChangelogOpen,
+  openModalChangelog,
+}) => {
+  const { t } = useTranslation(['common', 'changelog']);
+
+  const title = `${t('version')} ${modalChangelogItem.version}`;
+  return (
+    <Modal
+      isOpen={modalChangelogOpen}
+      onClose={() => openModalChangelog(false)}
+    >
+      <ModalBackdrop />
+      <ModalContent className="max-h-5/6">
+        <ModalHeader>
+          <Heading className="color-custom-text-modal">{title}</Heading>
+          <ModalCloseButton>
+            <Icon
+              as={CloseIcon}
+              size="md"
+              className="stroke-background-400 group-[:hover]/modal-close-button:stroke-background-700 group-[:active]/modal-close-button:stroke-background-900 group-[:focus-visible]/modal-close-button:stroke-background-900"
+            />
+          </ModalCloseButton>
+        </ModalHeader>
+        <ModalBody>
+          <Text>
+            {t(`${modalChangelogItem.id}.infos`, {
+              ns: 'changelog',
+              returnObjects: true,
+              joinArrays: '\n',
+            })}
+          </Text>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ChangelogModal;
