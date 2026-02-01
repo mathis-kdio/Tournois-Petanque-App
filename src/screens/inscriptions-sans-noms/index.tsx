4506b5c27ad63f417cf8ab1fbc6ade0bd505ae63
Mathis Cadio <mathis.cadio@gmail.com>
2026-02-01 15:49:05 +0100

    refacto inscriptions-sans-noms

---
+++ src/screens/inscriptions-sans-noms/index.tsx
@@ -0,0 +1,96 @@
+import { ScrollView } from '@/components/ui/scroll-view';
+import { Input, InputField } from '@/components/ui/input';
+import { Text } from '@/components/ui/text';
+import { VStack } from '@/components/ui/vstack';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import TopBarBack from '@/components/topBar/TopBarBack';
+import { PreparationTournoiModel } from '@/types/interfaces/preparationTournoiModel';
+import { usePreparationTournoi } from '@/repositories/preparationTournoi/usePreparationTournoi';
+import Loading from '@/components/Loading';
+import StartButton from './components/StartButton';
+
+const InscriptionsSansNoms = () => {
+  const { t } = useTranslation();
+
+  const { getActualPreparationTournoi } = usePreparationTournoi();
+
+  const [preparationTournoiModel, setPreparationTournoiModel] = useState<
+    PreparationTournoiModel | undefined
+  >(undefined);
+
+  const [nbJoueurNormaux, setNbJoueurNormaux] = useState(0);
+  const [nbJoueurEnfants, setNbJoueurEnfants] = useState(0);
+
+  const secondInput = React.createRef<any>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resultpreparationTournoi = await getActualPreparationTournoi();
+      setPreparationTournoiModel(resultpreparationTournoi);
+    };
+    fetchData();
+  }, [getActualPreparationTournoi]);
+
+  if (!preparationTournoiModel) {
+    return <Loading />;
+  }
+
+  const textInputJoueursNormaux = (text: string) =>
+    setNbJoueurNormaux(parseInt(text));
+
+  const textInputJoueursEnfants = (text: string) =>
+    setNbJoueurEnfants(parseInt(text));
+
+  return (
+    <ScrollView className="h-1 bg-custom-background">
+      <TopBarBack title={t('inscription_sans_noms_navigation_title')} />
+      <VStack space="2xl" className="flex-1 px-10">
+        <Text className="text-typography-white text-center text-xl">
+          {t('nombre_joueurs', { nb: nbJoueurNormaux + nbJoueurEnfants })}
+        </Text>
+        <VStack>
+          <Text className="text-typography-white text-md">
+            {t('nombre_joueurs_adultes')}{' '}
+          </Text>
+          <Input className="border-custom-bg-inverse">
+            <InputField
+              className="text-typography-white placeholder:text-typography-white"
+              placeholder={t('nombre_placeholder')}
+              keyboardType="number-pad"
+              returnKeyType="next"
+              autoFocus={true}
+              blurOnSubmit={false}
+              onChangeText={(text) => textInputJoueursNormaux(text)}
+              onSubmitEditing={() => secondInput.current.focus()}
+            />
+          </Input>
+        </VStack>
+        <VStack>
+          <Text className="text-typography-white text-md">
+            {t('nombre_joueurs_enfants')}{' '}
+          </Text>
+          <Input className="border-custom-bg-inverse">
+            <InputField
+              className="text-typography-white placeholder:text-typography-white"
+              placeholder={t('nombre_placeholder')}
+              keyboardType="number-pad"
+              onChangeText={(text) => textInputJoueursEnfants(text)}
+              ref={secondInput}
+            />
+          </Input>
+        </VStack>
+        <Text className="text-typography-white">
+          {t('joueurs_enfants_explication')}
+        </Text>
+        <StartButton
+          preparationTournoiModel={preparationTournoiModel}
+          nbJoueurNormaux={nbJoueurNormaux}
+          nbJoueurEnfants={nbJoueurEnfants}
+        />
+      </VStack>
+    </ScrollView>
+  );
+};
+
+export default InscriptionsSansNoms;
