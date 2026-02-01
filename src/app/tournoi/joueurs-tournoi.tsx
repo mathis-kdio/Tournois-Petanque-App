115c4a2e11b32ac7b181e9f22afc8b77e7cc6b91
Mathis Cadio <mathis.cadio@gmail.com>
2026-02-01 16:46:19 +0100

    joueurs-tournoi refacto

--- src/app/tournoi/joueurs-tournoi.tsx
+++ src/app/tournoi/joueurs-tournoi.tsx
@@ -1,93 +1,12 @@
-import { FlatList } from '@/components/ui/flat-list';
-import { Box } from '@/components/ui/box';
-import { Text } from '@/components/ui/text';
-import { VStack } from '@/components/ui/vstack';
-import { ButtonText, Button } from '@/components/ui/button';
-import ListeJoueurItem from '@components/ListeJoueurItem';
-import { useTranslation } from 'react-i18next';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import TopBarBack from '@/components/topBar/TopBarBack';
-import { JoueurModel } from '@/types/interfaces/joueurModel';
-import { ListRenderItem } from 'react-native';
-import { ModeTournoi } from '@/types/enums/modeTournoi';
-import { OptionsTournoiModel } from '@/types/interfaces/optionsTournoiModel';
-import { useRouter } from 'expo-router';
-import { useEffect, useState } from 'react';
-import { useTournois } from '@/repositories/tournois/useTournois';
-import { TournoiModel } from '@/types/interfaces/tournoi';
-import Loading from '@/components/Loading';
+import JoueursTournoi from '@/screens/joueurs-tournoi';
 
-const JoueursTournoi = () => {
-  const { t } = useTranslation();
-  const router = useRouter();
-  const { getActualTournoi } = useTournois();
-
-  const [tournoi, setTournoi] = useState<TournoiModel | undefined>(undefined);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const resultTournoi = await getActualTournoi();
-      setTournoi(resultTournoi);
-    };
-    fetchData();
-  }, [getActualTournoi]);
-
-  if (!tournoi) {
-    return <Loading />;
-  }
-
-  const _retourMatchs = () => {
-    router.navigate('/tournoi');
-  };
-
-  const _displayListeJoueur = (optionsTournoi: OptionsTournoiModel) => {
-    const { listeJoueurs, mode, typeEquipes, typeTournoi } = optionsTournoi;
-    if (listeJoueurs === undefined) {
-      return;
-    }
-    const renderItem: ListRenderItem<JoueurModel> = ({ item }) => (
-      <ListeJoueurItem
-        joueur={item}
-        isInscription={false}
-        avecEquipes={mode === ModeTournoi.AVECEQUIPES}
-        typeEquipes={typeEquipes}
-        modeTournoi={mode}
-        typeTournoi={typeTournoi}
-        nbJoueurs={listeJoueurs.length}
-        showCheckbox={true}
-        tournoiID={tournoi.tournoiId}
-        listesJoueurs={undefined}
-      />
-    );
-
-    return (
-      <FlatList
-        removeClippedSubviews={false}
-        data={optionsTournoi.listeJoueurs}
-        keyExtractor={(item: JoueurModel) => item.id.toString()}
-        renderItem={renderItem}
-      />
-    );
-  };
-
+const JoueursTournoiScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <VStack className="flex-1 bg-custom-background">
-        <TopBarBack title={t('liste_joueurs_inscrits_navigation_title')} />
-        <Text className="text-typography-white text-xl text-center">
-          {t('nombre_joueurs', { nb: tournoi.options.listeJoueurs.length })}
-        </Text>
-        <VStack className="flex-1 my-2">
-          {_displayListeJoueur(tournoi.options)}
-        </VStack>
-        <Box className="px-10 mb-2">
-          <Button action="primary" onPress={() => _retourMatchs()}>
-            <ButtonText>{t('retour_liste_matchs_bouton')}</ButtonText>
-          </Button>
-        </Box>
-      </VStack>
+      <JoueursTournoi />
     </SafeAreaView>
   );
 };
 
-export default JoueursTournoi;
+export default JoueursTournoiScreen;
