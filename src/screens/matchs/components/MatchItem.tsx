7ab8aa98b201920416fbd9bb5e472ac0360362ad
Mathis Cadio <mathis.cadio@gmail.com>
2026-02-12 21:51:00 +0100

    Refacto MatchItem

--- src/screens/matchs/components/MatchItem.tsx
+++ src/screens/matchs/components/MatchItem.tsx
@@ -1,31 +1,33 @@
-import { VStack } from '@/components/ui/vstack';
-import { Text } from '@/components/ui/text';
-import { HStack } from '@/components/ui/hstack';
-import { Divider } from '@/components/ui/divider';
 import { Box } from '@/components/ui/box';
+import { Divider } from '@/components/ui/divider';
+import { HStack } from '@/components/ui/hstack';
+import { Text } from '@/components/ui/text';
+import { VStack } from '@/components/ui/vstack';
+import { EquipeType } from '@/types/interfaces/equipeType';
 import { JoueurModel } from '@/types/interfaces/joueurModel';
 import { MatchModel } from '@/types/interfaces/matchModel';
 import { FontAwesome5 } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { TouchableOpacity } from 'react-native';
-import { useRouter } from 'expo-router';
 
 export interface Props {
   match: MatchModel;
 }
 
-type Equipe = 1 | 2;
+type Equipe = 0 | 1;
 
 const MatchItem: React.FC<Props> = ({ match }) => {
   const { t } = useTranslation();
   const router = useRouter();
 
-  const displayTitle = (match: MatchModel) => {
-    const { id, terrain, score1, score2 } = match;
+  const { matchId, equipe, terrain, score1, score2 } = match;
+
+  const displayTitle = () => {
     const score1Int = score1 ? score1 : 0;
     const score2Int = score2 ? score2 : 0;
-    const txt = terrain ? terrain.name : `${t('match_numero')}${id + 1}`;
+    const txt = terrain ? terrain.name : `${t('match_numero')}${matchId + 1}`;
     return (
       <HStack>
         <Box className="flex-1 items-center">
@@ -47,20 +49,14 @@ const MatchItem: React.FC<Props> = ({ match }) => {
     );
   };
 
-  const displayEquipe = (equipe: Equipe, match: MatchModel) => {
-    const nomsJoueurs = [];
-    const joueursEquipe = match.equipe[equipe - 1];
-    for (let i = 0; i < 4; i++) {
-      const joueur = joueursEquipe[i];
-      if (joueur && joueur !== -1) {
-        nomsJoueurs.push(displayName(joueur, equipe));
-      }
-    }
-    return nomsJoueurs;
+  const displayEquipe = (equipeType: EquipeType, equipeId: Equipe) => {
+    return equipeType
+      .filter((a) => !!a && a !== -1)
+      .map((b) => displayName(b, equipeId));
   };
 
   const displayName = (joueur: JoueurModel, equipe: Equipe) => {
-    if (equipe === 1) {
+    if (equipe === 0) {
       return (
         <Text
           key={joueur.joueurTournoiId}
@@ -83,35 +79,37 @@ const MatchItem: React.FC<Props> = ({ match }) => {
     }
   };
 
-  const displayScore = (match: MatchModel) => {
-    const score1 = match.score1 ?? '?';
-    const score2 = match.score2 ?? '?';
+  const displayScore = () => {
     return (
       <HStack className="justify-center">
-        <Text className="text-typography-white text-2xl p-2">{score1}</Text>
+        <Text className="text-typography-white text-2xl p-2">
+          {score1 ?? '?'}
+        </Text>
         <Text className="text-typography-white text-2xl p-2"> VS </Text>
-        <Text className="text-typography-white text-2xl p-2">{score2}</Text>
+        <Text className="text-typography-white text-2xl p-2">
+          {score2 ?? '?'}
+        </Text>
       </HStack>
     );
   };
 
-  const navigateMatchDetail = (idMatch: number) => {
+  const navigateMatchDetail = () => {
     router.navigate({
       pathname: '/tournoi/match-detail',
       params: {
-        idMatch: idMatch,
+        idMatch: match.matchId,
       },
     });
   };
 
   return (
-    <TouchableOpacity onPress={() => navigateMatchDetail(match.id)}>
+    <TouchableOpacity onPress={() => navigateMatchDetail()}>
       <VStack className="m-2">
-        {displayTitle(match)}
+        {displayTitle()}
         <HStack className="items-center">
-          <Box className="flex-1">{displayEquipe(1, match)}</Box>
-          <Box className="flex-1">{displayScore(match)}</Box>
-          <Box className="flex-1">{displayEquipe(2, match)}</Box>
+          <Box className="flex-1">{displayEquipe(equipe[0], 0)}</Box>
+          <Box className="flex-1">{displayScore()}</Box>
+          <Box className="flex-1">{displayEquipe(equipe[1], 1)}</Box>
         </HStack>
       </VStack>
       <Divider />
