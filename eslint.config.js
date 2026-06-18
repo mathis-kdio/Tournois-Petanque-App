55b0dd5ab1ce351e5d9aa46a9a8b2cc1e0216883
Mathis Cadio <mathis.cadio@gmail.com>
2026-06-18 22:36:46 +0200

    installation react doctor

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const { default: reactDoctor } = require('eslint-plugin-react-doctor');
 
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
+  reactDoctor.configs['react-native'],
   {
     ignores: [
       'dist/*',
