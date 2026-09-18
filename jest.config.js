bd5e31b5ef59670ff08f1aa0a08c47d05307ddf6
Mathis Cadio <mathis.cadio@gmail.com>
2026-09-18 23:43:55 +0200

    setup

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('jest-expo').Config} */
+const config = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/', '/dist/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@gluestack-ui|@legendapp|nativewind|react-native-reanimated|react-native-gesture-handler|react-native-screens|react-native-safe-area-context|react-native-pager-view|react-native-tab-view|react-native-worklets|drizzle-orm)',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@animations/(.*)$': '<rootDir>/src/animations/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
+    '^@screens/(.*)$': '<rootDir>/src/app/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  testEnvironment: 'node',
+};
+
+module.exports = config;
---
+++ jest.setup.js
@@ -0,0 +1,10 @@
+// Configuration Jest exécutée après chaque fichier de test.
+// Ajoutez ici les matchers personnalisés, les mocks globaux, etc.
+
+// Matchers supplémentaires pour les composants React Native
+import '@testing-library/jest-native';
+
+// Exemple de mock global (décommentez selon vos besoins) :
+// jest.mock('@react-native-async-storage/async-storage', () =>
+//   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+// );
